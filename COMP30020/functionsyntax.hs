184ec6d4e3aa3529df499bdbe7ff1a22645285bf
OpticGenius <dhaliwala@student.unimelb.edu.au>
2017-07-29 15:59:32 +1000

    made changes here

--- COMP30020/functionsyntax.hs
+++ COMP30020/functionsyntax.hs
@@ -54,3 +54,7 @@ tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " 
 length' :: (Num b) => [a] -> b
 length' [] = 0
 length' (_:xs) = 1 + length' xs
+
+sum :: [Int] -> Int
+sum [] = 0
+sum(x:xs) = x + sum xs
