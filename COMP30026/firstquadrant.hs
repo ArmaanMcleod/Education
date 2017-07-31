0cd4df15c1416c3d51758f38ca6f574931008efb
OpticGenius <dhaliwala@student.unimelb.edu.au>
2017-07-31 19:35:32 +1000

    Added some more code

--- COMP30026/firstquadrant.hs
+++ COMP30026/firstquadrant.hs
@@ -4,3 +4,9 @@ goodPoint :: Point -> Bool
 goodPoint (Pt x y)
     | x > 0 && y > 0 = True
     | otherwise = False
+
+--mymaximum :: List a -> Int
+--mymaximum (ListNode x xs)
+    --| x > maxTail = x 
+    --| otherwise = maxTail
+    --where maxTail = mymaximum xs
---
+++ COMP30026/linkedlist.hs
@@ -0,0 +1,9 @@
+data List a = ListNode a (List a) | ListEnd
+
+mymaximum :: (Ord a) => List a -> a
+mymaximum (ListNode x ListEnd) = x
+mymaximum (ListNode x xs)
+    | x > maxxs = x
+    | otherwise = maxxs
+    where maxxs = mymaximum xs
+
