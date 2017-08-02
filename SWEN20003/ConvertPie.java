7b871ade08ba3d63b02ef49a18f04b52db67a9be
OpticGenius <dhaliwala@student.unimelb.edu.au>
2017-08-02 21:48:55 +1000

    Conversion program added

--- SWEN20003/ConvertPie.java
+++ SWEN20003/ConvertPie.java
@@ -1,9 +1,11 @@
 import java.lang.Math;
 
+/* class for printing value of pie */
 public class ConvertPie {
 	public static void main(String args[]) {
 		double pie = Math.PI;
 
+		/* formatting and printing string */
 		String str = String.format("%03d%.4f", 0, pie);
 		String text = String.format("\"The value of \'pi\' to four places is %s\"", str);
 		System.out.println(text);
