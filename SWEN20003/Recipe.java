82b2392c31a017535e7bf175b06f245fa281240e
OpticGenius <dhaliwala@student.unimelb.edu.au>
2017-08-08 14:32:35 +1000

    I can calculate stuff!

---
+++ SWEN20003/Recipe.java
@@ -0,0 +1,30 @@
+public class Recipe {
+    
+    public static void main(String[] args) {
+        /* test string */
+        String recipe = "12 $12.72 Beef\n" +
+                        "04 Carrots will cost $03.64\n" +
+                        "99 I think $00.05 for Herbs";
+        
+        /* variable declarations */
+        double total = 0.0, price;
+        int quantity;
+        final int start = 0;
+        final int middle = 2;
+        final int end = 7;
+        
+        /* only extracting important contents of string */
+        recipe = recipe.replaceAll("[^0-9.]", "");
+        
+        /* calculating total recipe cost */
+        for (int i = 0; i < 3; i++) {
+            quantity = Integer.parseInt(recipe.substring(start,middle));
+            price = Double.parseDouble(recipe.substring(middle,end));
+            total += quantity * price;
+            recipe = recipe.substring(end);
+        }
+       
+        System.out.println("Total recipe price: " + total);
+        
+    }
+}
