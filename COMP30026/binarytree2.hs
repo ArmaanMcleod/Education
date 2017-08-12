3a0dcb0ee0cb12dc75986aee593e950cfb925f49
OpticGenius <dhaliwala@student.unimelb.edu.au>
2017-08-13 00:15:33 +1000

    Added more binary trees

--- COMP30026/binarytree2.hs
+++ COMP30026/binarytree2.hs
@@ -15,4 +15,30 @@ contents :: BinTree a -> [a]
 contents Void = []
 contents (Node a l r) = contents(l) ++ [a] ++ contents(r)
 
+insertbst :: (Ord a) => a -> BinTree a -> BinTree a
+insertbst x Void = Node x Void Void
+insertbst x (Node y l r)
+    | y == x = Node y l r
+    | y < x = Node y l (insertbst x r)
+    | y > x = Node y (insertbst x l) r
+
+buildbst :: (Ord a) => [a] -> BinTree a
+buildbst [] = Void
+buildbst (x:xs) = insertbst x (buildbst xs)
+
+buildbst' :: (Ord a) => [a] -> BinTree a
+buildbst' xs = foldl (\acc x -> insertbst x acc) Void (reverse xs)
+
+
+
+
+
+
+    
+
+
+
+    
+    
+
 
