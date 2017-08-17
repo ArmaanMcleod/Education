917e9b7c475d787d3d31b03aeee22728266aa846
OpticGenius <dhaliwala@student.unimelb.edu.au>
2017-08-17 15:27:22 +1000

    made more changes to file

--- SWEN20003/CaeserCipher.java
+++ SWEN20003/CaeserCipher.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class CaeserCipher {
 
-  /* Constants used in program */
 	public static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	public static final String END = "Stop";
 	public static final String ENCODE = "Encode";
@@ -11,13 +10,10 @@ public class CaeserCipher {
 	public static final int MAX_LETTERS = ALPHABET.length();
 
 	public static void main(String[] args) {
-
-        /* Created scanner object for reading input */
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.print("Encoding/Decoding? ");
 
-        /* Reads input until CTRL-D is entered */
 		while (scanner.hasNextLine()) {
 
             /* Reads in direction */
@@ -26,23 +22,18 @@ public class CaeserCipher {
 				break;
 			}
 
-            /* Reads in number of shifts needed for decoding/encoding */
 			System.out.print("Enter n: ");
 			int n = scanner.nextInt();
 			scanner.nextLine();
 
-            /* Reads in text to encode/decode */
 			System.out.print("Enter text to convert: ");
 			String text = scanner.nextLine();
 
-            /* Converts text to all uppercase and whitespace is stripped */
 			text = text.replace(" ", "");
 			text = text.toUpperCase();
   			
-            /* If encoding is needed */
   			if (direction.equals(ENCODE)) {
 
-                /* Appends new encoded characters to a new buffer */
   				String encoding = "";
   				for (int i = 0; i < text.length(); i++) {
   					int charPosition = ALPHABET.indexOf(text.charAt(i));
@@ -54,10 +45,8 @@ public class CaeserCipher {
   				System.out.println(text);
   				System.out.println(encoding);
 
-            /* If decoding is needed */
   			} else if (direction.equals(DECODE)) {
 
-                /* Appends new decoded characters to a new buffer */
   				String decoding = "";
   				for (int i = 0; i < text.length(); i++) {
   					int charPosition = ALPHABET.indexOf(text.charAt(i));
