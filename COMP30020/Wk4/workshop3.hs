90f52f19f8dd8c370bf53fdc3390c268f9268ac5
OpticGenius <dhaliwala@student.unimelb.edu.au>
2017-08-18 20:47:38 +1000

    Attempted workshop questions

--- COMP30020/Wk4/workshop3.hs
+++ COMP30020/Wk4/workshop3.hs
@@ -1,16 +1,45 @@
+import Data.List
+
+-- Binary search tree type declaration
+data Tree k v = Leaf | Node k v (Tree k v) (Tree k v)
+    deriving (Eq, Show)
+
+-- eval expression
+data Expression 
+    = Var Variable
+    | Num Integer
+    | Plus Expression Expression
+    | Minus Expression Expression
+    | Times Expression Expression
+    | Div Expression Expression
+
+data Variable = A | B
+
 -- Question 2
 ftoc :: Double -> Double
 ftoc f = ((5 * 1.0) / 9) * (f - 32)
 
 -- Question 3
 quadRoots :: Double -> Double -> Double -> [Double]
-quadRoots a b c = [x1, x2]
-    where
-      x1 = (-b + ((b^2) - 4 * a * c) * 1.0) / 2 * a
-      x2 = (-b - ((b^2) - 4 * a * c) * 1.0) / 2 * a
+quadRoots a b c
+    | det > 0 = [left + right, left - right]
+    | det == 0 = [left]
+    | otherwise = []
+    where 
+      det   = b * b - 4 * a * b
+      left  = -b / denom
+      right = sqrt(det/denom)
+      denom = 2 * a
+
+isSorted' :: (Ord a) => [a] -> Bool
+isSorted' [_] = True
+isSorted' (x:xs)
+    | x <= head xs = isSorted' xs
+    | otherwise = False
 
 -- Question 4
-merge :: (Ord a) => [a] -> [a] -> [a]WW
+--  Merging two sorted lists
+merge :: (Ord a) => [a] -> [a] -> [a]
 merge [] [] = []
 merge [] xs = xs
 merge ys [] = ys
@@ -26,3 +55,16 @@ qsort (pivot:others) = qsort(lowers) ++ [pivot] ++ qsort(highers)
           highers = filter (>=pivot) others
 
 -- Question 6
+--data Tree k v = Leaf | Node k v (Tree k v) (Tree k v)
+  --deriving (Eq, Show)
+
+same_shape :: Tree a b -> Tree c d -> Bool
+same_shape Leaf Leaf = True
+same_shape (Node _ _ l1 r1) (Node _ _ l2 r2) = same_shape l1 l2 && same_shape r1 r2
+same_shape _ _ = False
+
+
+
+
+
+
