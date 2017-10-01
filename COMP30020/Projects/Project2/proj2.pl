c533969e97fc8d0bc56557dcd4fb53932ebde3d5
OpticGenius <dhaliwala@student.unimelb.edu.au>
2017-10-01 20:44:55 +1100

    Updated project

--- COMP30020/Projects/Project2/proj2.pl
+++ COMP30020/Projects/Project2/proj2.pl
@@ -2,50 +2,40 @@
 :- ensure_loaded(library(clpfd)).
 
 % 2 x 2 Solver
-puzzle_solution([[_, R02, R03], 
-               [R11, R12, R13], 
-               [R21, R22, R23]]) :-
+puzzle_solution(Puzzle) :-
+    (
+        length(Puzzle, 3),
+        diagonal_2_by_2(Puzzle)
+    ;
+        length(Puzzle, 4),
+        diagonal_3_by_3(Puzzle)
+    ;
+        length(Puzzle, 5),
+        diagonal_4_by_4(Puzzle)
+   ),
 
-    is_equal(R12, R23),
-    is_valid_heading([R11, R12, R13]),
-    is_valid_heading([R21, R22, R23]),
-    is_valid_heading([R02, R12, R22]),
-    is_valid_heading([R03, R13, R23]).
+    Puzzle = [_|T],
+    maplist(is_valid_heading, T),
+    transpose(Puzzle, [_|T2]),
+    maplist(is_valid_heading, T2).
 
-% 3 x 3 Solver
-puzzle_solution([[_, R02, R03, R04],
-               [R11, R12, R13, R14],
-               [R21, R22, R23, R24],
-               [R31, R32, R33, R34]]) :-
+diagonal_2_by_2([[_, _, _],
+                 [_, R12, _],
+                 [_, _, R23]]) :-
+    all_same([R12, R23]).
 
-    is_equal(R12, R23), 
-    is_equal(R23, R34),
-    is_valid_heading([R11, R12, R13, R14]),
-    is_valid_heading([R21, R22, R23, R24]),
-    is_valid_heading([R31, R32, R33, R34]),
-    is_valid_heading([R02, R12, R22, R32]),
-    is_valid_heading([R03, R13, R23, R33]),
-    is_valid_heading([R04, R14, R24, R34]).
+diagonal_3_by_3([[_, _, _, _],
+                 [_, R12, _, _],
+                 [_, _, R23, _],
+                 [_, _ ,_, R34]]) :-
+    all_same([R12, R23, R34]).
 
-% 4 x 4 Solver
-puzzle_solution([[_, R02, R03, R04, R05],
-               [R11, R12, R13, R14, R15],
-               [R21, R22, R23, R24, R25],
-               [R31, R32, R33, R34, R35],
-               [R41, R42, R43, R44, R45]]) :-
-
-    is_equal(R12, R23),
-    is_equal(R23, R34),
-    is_equal(R34, R45),
-    is_valid_heading([R11, R12, R13, R14, R15]),
-    is_valid_heading([R21, R22, R23, R24, R25]),
-    is_valid_heading([R31, R32, R33, R34, R35]),
-    is_valid_heading([R41, R42, R43, R44, R45]),
-    is_valid_heading([R02, R12, R22, R32, R42]),
-    is_valid_heading([R03, R13, R23, R33, R43]),
-    is_valid_heading([R04, R14, R24, R34, R44]),
-    is_valid_heading([R05, R15, R25, R35, R45]).
-
+diagonal_4_by_4([[_, _, _, _, _],
+                 [_, R12, _, _, _],
+                 [_, _, R23, _, _],
+                 [_, _, _, R34, _],
+                 [_, _, _, _, R45]]) :-
+    all_same([R12, R23, R34, R45]).
 
 % Checks if a heading holds the sum of product of the given
 % row or column
@@ -77,6 +67,8 @@ check_sums([X|Xs]) :- sum_list(Xs, X).
 product(A, B, P) :- P is A * B.
 check_product(Xs, P) :- foldl(product, Xs, 1, P).
 
-% Checks if two elements are equal.
-is_equal(X, Y) :- X = Y.
+% Checks if all elements are equal
+all_same([]).   
+all_same([_]).
+all_same([X, X|T]) :- all_same([X|T]).
 
